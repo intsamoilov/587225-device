221803c8d7c678c85437c03a34edae01fbf79f69
Alexander Samoilov <39725294+intsamoilov@users.noreply.github.com>
2018-07-02 14:52:52 +0300

    Оживил слайдер, исправил недочеты

--- js/script.js
+++ js/script.js
@@ -17,6 +17,19 @@ var isStorageSupport = true;
 var storageName = "";
 var storageEmail = "";
 
+var sliderPopular = document.querySelector(".slides");
+var activeSlidePopular = sliderPopular.querySelector(".slide.active");
+var btnsPopular = sliderPopular.querySelectorAll(".slider-btn");
+
+var sliderServices = document.querySelector(".main-services");
+var activeSlideServices = sliderServices.querySelector(".main-services-info.active");
+var btnsServices = sliderServices.querySelectorAll(".services-btn");
+var activeBtnServices = sliderServices.querySelector(".selected");
+
+/*var btnsServices1 = sliderServices.querySelector(".services-btn-1");
+var btnsServices2 = sliderServices.querySelector(".services-btn-2");
+var btnsServices3 = sliderServices.querySelector(".services-btn-3");*/
+
 try {
 	storageName = localStorage.getItem("name");
 	storageEmail = localStorage.getItem("email");
@@ -43,6 +56,7 @@ modalFormClose.addEventListener("click", function(evt) {
 	modalForm.classList.remove("modal-show");
 	modalForm.classList.remove("modal-error");
 });
+
 formFeedback.addEventListener("submit", function(evt) {
 	if (!formName.value || !formEmail.value) {
 		evt.preventDefault();
@@ -69,11 +83,13 @@ mapLink.addEventListener("click", function(evt) {
 	modal.classList.add("modal-show");
 	modalMap.classList.add("modal-show");
 });
+
 modalMapClose.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	modal.classList.remove("modal-show");
 	modalMap.classList.remove("modal-show");
 });
+
 window.addEventListener("keydown", function(evt) {
 	if (evt.keyCode == 27 && modalMap.classList.contains("modal-show")) {
 		evt.preventDefault();
@@ -93,3 +109,33 @@ function init(){
 	});
 	myMap.geoObjects.add(myPlacemark);
 }
+
+var onBtnClickPopular = function(evt) {
+	evt.preventDefault();
+	var slideKeyPopular = "." + evt.target.dataset.key; 
+
+	activeSlidePopular.classList.remove("active");   
+	activeSlidePopular = sliderPopular.querySelector(slideKeyPopular);
+	activeSlidePopular.classList.add("active");
+};
+
+[].forEach.call(btnsPopular, function(btn) {
+	btn.addEventListener("click", onBtnClickPopular);
+});
+
+var onBtnClickServices = function(evt) {
+	evt.preventDefault();
+	var slideKeyServices = "." + evt.target.dataset.key; 
+
+	activeSlideServices.classList.remove("active");   
+	activeSlideServices = sliderServices.querySelector(slideKeyServices);
+	activeSlideServices.classList.add("active");
+
+	activeBtnServices.classList.remove("selected");
+	activeBtnServices = evt.target;
+	activeBtnServices.classList.add("selected");
+};
+
+[].forEach.call(btnsServices, function(btn) {
+	btn.addEventListener("click", onBtnClickServices);
+});
